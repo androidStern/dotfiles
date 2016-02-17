cc61648c1560e636087c48a03315036e13d34ecb
Andrew Stern <winterfell@Andrews-MacBook-Pro.local>
2016-02-16 21:34:04 -0800

    Cleanup

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,17 +3,21 @@
 # Don't change. The following determines where YADR is installed.
 yadr=$HOME/.yadr
 
+# Get operating system
+platform='unknown'
+unamestr=$(uname)
+if [[ $unamestr == 'Linux' ]]; then
+  platform='linux'
+elif [[ $unamestr == 'Darwin' ]]; then
+  platform='darwin'
+fi
+
 # YADR support
 alias yav='yadr vim-add-plugin'
 alias ydv='yadr vim-delete-plugin'
 alias ylv='yadr vim-list-plugin'
 alias yup='yadr update-plugins'
 alias yip='yadr init-plugins'
-
-# TODO
-alias td='/usr/local/bin/todo.sh'
-alias tda='/usr/local/bin/todo.sh add'
-
 
 # PS
 alias psa="ps aux"
@@ -22,21 +26,31 @@ alias psr='ps aux | grep ruby'
 
 # Moving around
 alias cdb='cd -'
-alias cdd='cd ~/Developer'
-alias cdw="cd ~/Developer/Wip"
+alias cls='clear;ls'
 
 # Show human friendly numbers and colors
 alias df='df -h'
-alias ll='ls -alGh'
-alias ls='ls -Gh'
-# alias du='du -h -d 2'
+alias du='du -h -d 2'
+
+if [[ $platform == 'linux' ]]; then
+  alias ll='ls -alh --color=auto'
+  alias ls='ls --color=auto'
+elif [[ $platform == 'darwin' ]]; then
+  alias ll='ls -alGh'
+  alias ls='ls -Gh'
+fi
 
 # show me files matching "ls grep"
 alias lsg='ll | grep'
 
 # Alias Editing
+TRAPHUP() {
+  source $yadr/zsh/aliases.zsh
+}
+
 alias ae='vim $yadr/zsh/aliases.zsh' #alias edit
 alias ar='source $yadr/zsh/aliases.zsh'  #alias reload
+alias gar="killall -HUP -u \"$USER\" zsh"  #global alias reload
 
 # vim using
 mvim --version > /dev/null 2>&1
@@ -45,12 +59,14 @@ if [ $MACVIM_INSTALLED -eq 0 ]; then
   alias vim="mvim -v"
 fi
 
+# mimic vim functions
+alias :q='exit'
+
 # vimrc editing
 alias ve='vim ~/.vimrc'
 
 # zsh profile editing
 alias ze='vim ~/.zshrc'
-alias zr='source ~/.zshrc'
 
 # Git Aliases
 alias gs='git status'
@@ -68,6 +84,7 @@ alias gci='git ci'
 alias gco='git co'
 alias gcp='git cp'
 alias ga='git add -A'
+alias gap='git add -p'
 alias guns='git unstage'
 alias gunc='git uncommit'
 alias gm='git merge'
@@ -89,13 +106,15 @@ alias gfch='git fetch'
 alias gd='git diff'
 alias gb='git b'
 alias gbd='git b -D -w'
+# Staged and cached are the same thing
 alias gdc='git diff --cached -w'
+alias gds='git diff --staged -w'
 alias gpub='grb publish'
 alias gtr='grb track'
 alias gpl='git pull'
 alias gplr='git pull --rebase'
 alias gps='git push'
-alias gpsh='git push'
+alias gpsh='git push -u origin `git rev-parse --abbrev-ref HEAD`'
 alias gnb='git nb' # new branch aka checkout -b
 alias grs='git reset'
 alias grsh='git reset --hard'
@@ -108,9 +127,7 @@ alias gsmu='git submodule update'
 alias gt='git t'
 alias gbg='git bisect good'
 alias gbb='git bisect bad'
-alias gum='git checkout master && git fetch && git pull origin master'
-alias gfar='git fetch origin && git rebase -i origin/master'
-alias gbr="git for-each-ref --count=15 --sort=-committerdate refs/heads/ --format='%(objectname:short) %(refname:short)'"
+alias gdmb='git branch --merged | grep -v "\*" | xargs -n 1 git branch -d'
 
 # Common shell functions
 alias less='less -r'
@@ -124,6 +141,14 @@ alias cl='clear'
 alias gz='tar -zcvf'
 
 # Ruby
+alias c='rails c' # Rails 3
+alias co='script/console' # Rails 2
+alias cod='script/console --debugger'
+
+#If you want your thin to listen on a port for local VM development
+#export VM_IP=10.0.0.1 <-- your vm ip
+alias ts='thin start -a ${VM_IP:-127.0.0.1}'
+alias ms='mongrel_rails start'
 alias tfdl='tail -f log/development.log'
 alias tftl='tail -f log/test.log'
 
@@ -132,6 +157,11 @@ alias k9='kill -9'
 
 # Gem install
 alias sgi='sudo gem install --no-ri --no-rdoc'
+
+# TODOS
+# This uses NValt (NotationalVelocity alt fork) - http://brettterpstra.com/project/nvalt/
+# to find the note called 'todo'
+alias todo='open nvalt://find/todo'
 
 # Forward port 80 to 3000
 alias portforward='sudo ipfw add 1000 forward 127.0.0.1,3000 ip from any to any 80 in'
@@ -142,46 +172,39 @@ alias rdmr='rake db:migrate:redo'
 # Zeus
 alias zs='zeus server'
 alias zc='zeus console'
+alias zr='zeus rspec'
+alias zrc='zeus rails c'
+alias zrs='zeus rails s'
+alias zrdbm='zeus rake db:migrate'
+alias zrdbtp='zeus rake db:test:prepare'
 
 # Rspec
+alias rs='rspec spec'
+alias sr='spring rspec'
+alias src='spring rails c'
 alias srgm='spring rails g migration'
 alias srdm='spring rake db:migrate'
 alias srdt='spring rake db:migrate'
 alias srdmt='spring rake db:migrate db:test:prepare'
 
-# Cucumber
-alias cuke='bin/rake pace:reset_test_accounts && bin/bundle exec cucumber -p headless'
+
+# Sprintly - https://github.com/nextbigsoundinc/Sprintly-GitHub
+alias sp='sprintly'
+# spb = sprintly branch - create a branch automatically based on the bug you're working on
+alias spb="git checkout -b \`sp | tail -2 | grep '#' | sed 's/^ //' | sed 's/[^A-Za-z0-9 ]//g' | sed 's/ /-/g' | cut -d"-" -f1,2,3,4,5\`"
+
+alias hpr='hub pull-request'
+alias grb='git recent-branches'
 
 # Finder
 alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
 alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
 
-# Redo last command with sudo
-alias redo='sudo \!-1'
+alias dbtp='spring rake db:test:prepare'
+alias dbm='spring rake db:migrate'
+alias dbmr='spring rake db:migrate:redo'
+alias dbmd='spring rake db:migrate:down'
+alias dbmu='spring rake db:migrate:up'
 
-# Bundler
-alias be='bundle exec'
-
-# Node Webkit
-alias nw="open -n -a node-webkit"
-
-# Gobbler Specific
-alias cdg="cd ~/src/mam/server/mam"
-alias cdm="cd ~/src/gob_marketplace"
-alias sshu="ssh 'ubuntu@gobbler-dev.com'"
-alias railss="bin/rails s"
-alias railsc="bin/rails c"
-alias gspot="sudo ~/gob_small_spawn"
-alias stopPace='launchctl unload /usr/local/opt/gob_pace_server/homebrew.mxcl.gob_pace_server.plist'
-alias startPace='launchctl load /usr/local/opt/gob_pace_server/homebrew.mxcl.gob_pace_server.plist'
-alias mktStop='launchctl unload /usr/local/opt/gob_marketplace/homebrew.mxcl.gob_marketplace.plist'
-alias mamStop='launchctl unload /usr/local/opt/gob_mam/homebrew.mxcl.gob_mam.plist'
-
-
-# Misc
-alias scratchme="rm -rf ./scratch && mkdir scratch && cd scratch"
-alias ic="ruby ~/Developer/interviewcake/bin/problem_template.rb"
-
-#  Find and kill all running rails processes
-alias railsk="ps aux  | grep [b]in/rails | awk '{print \$2}' | xargs kill -9"
-
+# Homebrew
+alias brewu='brew update  && brew upgrade --all && brew cleanup && brew prune && brew doctor'
